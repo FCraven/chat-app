6b1afd397b2e6991f3549a01ad2f65b6441f1f95
FCraven <craven.francis@gmail.com>
2020-11-25 17:52:29 -0500

    added users reducer

--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -1,5 +1,25 @@
-const users =()=> {
-  return {};
+import * as types from '../actions/actionTypes'
+
+
+const users =(state = [], action)=> {
+
+  switch(action.type){
+
+    case types.ADD_USER:
+      return state.concat([
+        {
+          name: action.name,
+          id: action.id
+        }
+      ])
+      
+    case types.USERS_LIST:
+      return action.user
+
+    default:
+      return state
+
+  }
 }
 
 export default users;
