3d7affc27d4d4c9449f87e389d8784b80f864be1
FCraven <craven.francis@gmail.com>
2020-11-25 11:56:30 -0500

    redux - added actions addMessage, addUser, messageReceived, populateUsersList

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,29 @@
 import * as types from './actionTypes'
 
 let nextMessageId = 0;
-const nextUserId = 0;
+let nextUserId = 0;
 
 export const addMessage=(message, author)=> ({
-  type: types.ADD_MESSAGE
+  type: types.ADD_MESSAGE,
+  id: nextMessageId++,
+  message,
+  author
+})
+
+export const addUser = name => ({
+  type: types.ADD_USER,
+  id: nextUserId++,
+  name
+})
+
+export const messageReceived =(message, author)=> ({
+  type: types.MESSAGE_RECEIVED,
+  id: nextMessageId++,
+  message,
+  author
+})
+
+export const populateUsersList = users => ({
+  type: types.USERS_LIST,
+  users
 })
